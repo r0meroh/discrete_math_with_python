c677387459d82e5c8641e28086f3cdf01fabbbbc
r0meroh <romeroh862@gmail.com>
2020-06-17 19:04:43 -0700

    created file

---
+++ non_colliding_queens.py
@@ -0,0 +1,12 @@
+# this excercise calculates how many queens on a given sized chessboard
+# can exist at once without their paths causing a collision.
+
+import itertools as itr
+
+def is_solution(permutation):
+    for(x1, x2) in itr.combinations(range(len(permutation)), 2):
+        if abs(x1 - x2) == abs(permutation[x1] - permutation[x2]):
+            return False
+    return True
+
+print(assert(is_solution([1,3,0,2]) == True))
